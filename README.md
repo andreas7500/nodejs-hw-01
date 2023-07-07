aa038c8755c6345d09bea31cb9e451e7a9a65e34
andreas7500 <andreas2875@gmail.com>
2023-07-08 00:04:07 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # nodejs-hw-01
+
+1. list
+   node index.js --action="list"
+   https://monosnap.com/file/3B6FCGuOXESnoCbTWviQQs1JEAGGLN
+
+2. get
+   node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+   https://monosnap.com/file/1sITYETb9hfo7FrBclrpi4BYcljj5e
+
+3. get null
+   https://monosnap.com/file/weF8qgdBROjJKMYznkGNAlj0JCD5p8
+
+4. add
+   node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+   https://monosnap.com/file/OvmybTza76HryLsdYnOEwOYIKGn0tY
+
+5. remove null
+   https://monosnap.com/file/vRYDrPgFxyGlEO1JJPeRIMVzlsonLT
+
+6. remove
+   node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH  
+   https://monosnap.com/file/bGrc6mtFUvHAiQac6irTDrrPg7FJ5X
--- contacts.js
+++ contacts.js
@@ -1,25 +1,57 @@
-// contacts.js
+import { nanoid } from "nanoid";
+import path from "path";
 
-/*
- * Розкоментуй і запиши значення
- * const contactsPath = ;
- */
-const path = require("path");
-const fs = require("fs/promises");
+import fs from "fs/promises";
 
-// TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код. Повертає масив контактів.
-}
+const filePath = path.resolve("./db/contacts.json");
 
-function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-}
+export const listContacts = async () => {
+  try {
+    const data = await fs.readFile(filePath);
+    console.log(data);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-}
+export const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find((item) => item.id === contactId);
+    if (!result) {
+      return null;
+    }
 
-function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту.
-}
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const removeContact = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((item) => item.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(filePath, JSON.stringify(contacts, null, 2));
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const addContact = async (data) => {
+  try {
+    const contacts = await listContacts();
+    const newContact = { id: nanoid(), ...data };
+    contacts.push(newContact);
+    await fs.writeFile(filePath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.error(error);
+  }
+};
